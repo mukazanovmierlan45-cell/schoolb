fcd91e07b594862091736f34f338577f2b18816c
Student <student@schoolb.loc>
2026-09-19 13:25:21 +0000

    Упражнение 2: вывод даты через переменные + год в футере

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+  // Установка локали и выбор значений даты
+  setlocale(LC_ALL, "russian");
+  $day = strftime('%d');
+  $mon = strftime('%B');
+  $year = strftime('%Y');
+?>
 <!DOCTYPE html>
 <html>
 
@@ -22,7 +29,7 @@
     <!-- Заголовок -->
     <!-- Область основного контента -->
     <blockquote>
-      <?php echo strftime('Сегодня %d-%m-%Y'); ?>
+      <?php echo 'Сегодня ', $day, ' число, ', $mon, ' месяц, ', $year, ' год.'; ?>
     </blockquote>
     <h3>Зачем мы ходим в школу?</h3>
     <p>
@@ -58,7 +65,7 @@
   </div>
   <div id="footer">
     <!-- Нижняя часть страницы -->
-    &copy; Супер Мега Веб-мастер, 2000 &ndash; 2021
+    &copy; Супер Мега Веб-мастер, 2000 &ndash; <?= $year ?>
     <!-- Нижняя часть страницы -->
   </div>
 </body>
